c79492e72f428dda24bb0e4be45a53fa07b7700e
SomiaAmer <105312434+somia213@users.noreply.github.com>
2024-07-19 21:39:50 +0300

    initial commit

---
+++ Chat/Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Somia on 19/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
